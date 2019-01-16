aeb5e65e27a0be7095ecc67797098ccd8dd2ee33
Nick Wells <wells.nicholas@gmail.com>
2019-01-16 16:51:31 +0000

    add PanicCheckStringWithStack and ReportUnexpectedPanic

--- testhelper/panicCheck.go
+++ testhelper/panicCheck.go
@@ -24,6 +24,41 @@ func PanicCheckString(t *testing.T, testID string,
 	return panicIsBad
 }
 
+// PanicCheckStringWithStack tests the panic value (which should be a string)
+// against the passed values. A stack trace should also be passed which will
+// be printed if the panic is not as expected and a panic was seen
+func PanicCheckStringWithStack(t *testing.T, testID string,
+	panicked, panicExpected bool,
+	panicVal interface{}, shouldContain []string, stackTrace []byte) bool {
+	t.Helper()
+
+	panicIsBad, msg :=
+		badPanicString(panicked, panicExpected, panicVal, shouldContain)
+	if panicIsBad {
+		t.Log(testID)
+		if panicked {
+			t.Logf("\t: %v\n", panicVal)
+			t.Log(string(stackTrace))
+		}
+		t.Errorf("\t: %s", msg)
+	}
+	return panicIsBad
+}
+
+// ReportUnexpectedPanic will check if panicked is true and will report the
+// unexpected panic if true. it returns the panicked value
+func ReportUnexpectedPanic(t *testing.T, testID string,
+	panicked bool, panicVal interface{}, stackTrace []byte) bool {
+	if panicked {
+		t.Log(testID)
+		t.Logf("\t: panic: %v\n", panicVal)
+		t.Log("\t: At:", string(stackTrace))
+		t.Error("\t: An unexpected panic was seen")
+	}
+
+	return panicked
+}
+
 // badPanicString checks whether the panic is unexpected in some way and
 // returns true and some explanatory message if so, false otherwise
 func badPanicString(panicked, panicExpected bool,
