c14b1a4a0aa7c4e2a6972eedf974ae6853a1cfe2
Nick Wells <wells.nicholas@gmail.com>
2023-05-23 18:29:37 +0100

    test: add test of FakeIO.Done error status

--- testhelper/fakeIO_test.go
+++ testhelper/fakeIO_test.go
@@ -12,87 +12,94 @@ import (
 func TestFakeIO(t *testing.T) {
 	testCases := []struct {
 		testhelper.ID
-		input    string
-		expOut   string
-		expErr   string
-		testFunc func()
+		testhelper.ExpErr
+		input     string
+		expStdout string
+		expStderr string
+		testFunc  func()
 	}{
 		{
 			ID:       testhelper.MkID("no input, no output"),
 			testFunc: func() {},
 		},
 		{
-			ID:       testhelper.MkID("no input, write to stdout"),
-			testFunc: func() { fmt.Println("Hello, World!") },
-			expOut:   "Hello, World!\n",
+			ID:        testhelper.MkID("no input, write to stdout"),
+			testFunc:  func() { fmt.Println("Hello, World!") },
+			expStdout: "Hello, World!\n",
 		},
 		{
-			ID:       testhelper.MkID("no input, write to stderr"),
-			testFunc: func() { fmt.Fprintln(os.Stderr, "Hello, World!") },
-			expErr:   "Hello, World!\n",
-		},
-		{
-			ID: testhelper.MkID("small input, write to stdout"),
-			testFunc: func() {
-				b := make([]byte, 5)
-				os.Stdin.Read(b)
-				os.Stdout.Write(b)
-			},
-			input:  "Hello, World!\n",
-			expOut: "Hello",
+			ID:        testhelper.MkID("no input, write to stderr"),
+			testFunc:  func() { fmt.Fprintln(os.Stderr, "Hello, World!") },
+			expStderr: "Hello, World!\n",
 		},
 		{
 			ID: testhelper.MkID("small input, write to stdout"),
 			testFunc: func() {
 				b := make([]byte, 5)
-				os.Stdin.Read(b)
-				os.Stderr.Write(b)
-			},
-			input:  "Hello, World!\n",
-			expErr: "Hello",
-		},
-		{
-			ID: testhelper.MkID("very large input, write to stdout"),
-			testFunc: func() {
-				b := make([]byte, 5)
-				os.Stdin.Read(b)
+				_, _ = os.Stdin.Read(b)
 				os.Stdout.Write(b)
 			},
-			input:  strings.Repeat("Hello, World!\n", 1024*1024),
-			expOut: "Hello",
+			input:     "Hello, World!\n",
+			expStdout: "Hello",
+		},
+		{
+			ID: testhelper.MkID("small input, write to stdout"),
+			testFunc: func() {
+				b := make([]byte, 5)
+				_, _ = os.Stdin.Read(b)
+				os.Stderr.Write(b)
+			},
+			input:     "Hello, World!\n",
+			expStderr: "Hello",
 		},
 		{
 			ID: testhelper.MkID("very large input, write to stdout"),
 			testFunc: func() {
 				b := make([]byte, 5)
-				os.Stdin.Read(b)
+				_, _ = os.Stdin.Read(b)
+				os.Stdout.Write(b)
+			},
+			input:     strings.Repeat("Hello, World!\n", 1024*1024),
+			expStdout: "Hello",
+		},
+		{
+			ID: testhelper.MkID("very large input, write to stdout"),
+			testFunc: func() {
+				b := make([]byte, 5)
+				_, _ = os.Stdin.Read(b)
 				os.Stderr.Write(b)
 			},
-			input:  strings.Repeat("Hello, World!\n", 1024*1024),
-			expErr: "Hello",
+			input:     strings.Repeat("Hello, World!\n", 1024*1024),
+			expStderr: "Hello",
 		},
 		{
 			ID: testhelper.MkID("very large input, close Stdin"),
+			ExpErr: testhelper.MkExpErr(
+				"Error writing to stdin",
+				"broken pipe"),
 			testFunc: func() {
 				b := make([]byte, 5)
-				os.Stdin.Read(b)
+				_, _ = os.Stdin.Read(b)
 				os.Stdin.Close()
 				os.Stderr.Write(b)
 			},
-			input:  strings.Repeat("Hello, World!\n", 1024*1024),
-			expErr: "Hello",
+			input:     strings.Repeat("Hello, World!\n", 1024*1024),
+			expStderr: "Hello",
 		},
 	}
 
 	for _, tc := range testCases {
 		fio, err := testhelper.NewStdioFromString(tc.input)
 		if err != nil {
-			t.Fatal("Unexpected error: ", err)
+			t.Fatal("Unexpected error (NewStdioFromString): ", err)
 		}
 		tc.testFunc()
 		actOut, actErr, err := fio.Done()
-		testhelper.DiffString(t, tc.IDStr(), "out", string(actOut), tc.expOut)
-		testhelper.DiffString(t, tc.IDStr(), "err", string(actErr), tc.expErr)
+		testhelper.CheckExpErr(t, err, tc)
+		testhelper.DiffString(t, tc.IDStr(), "stdout",
+			string(actOut), tc.expStdout)
+		testhelper.DiffString(t, tc.IDStr(), "stderr",
+			string(actErr), tc.expStderr)
 
 	}
 }
