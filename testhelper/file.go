680e15ba050a014e2dc97788ac31559defd0cb05
Nick Wells <wells.nicholas@gmail.com>
2023-09-14 19:07:23 +0100

    feature: add the MakeTempDirCopy func

--- testhelper/file.go
+++ testhelper/file.go
@@ -1,6 +1,11 @@
 package testhelper
 
-import "os"
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+)
 
 // MakeTempDir creates a directory with the given name and permissions and
 // returns a function that will delete the directory. This can be used as a
@@ -46,4 +51,70 @@ func TempChmod(name string, perms os.FileMode) func() {
 	}
 
 	return func() { _ = os.Chmod(name, fi.Mode()&os.ModePerm) }
+}
+
+// MakeTempDirCopy creates a temporary directory and copies the contents of
+// fromDir into it. It returns the name of the temporary directory, a
+// function to clean up (remove the temp dir) and any error encountered.
+// Only regular files and sub-directories can be copied. Any other
+// types of file in the from-directory will cause an error.
+func MakeTempDirCopy(fromDir string) (string, func() error, error) {
+	tmpDir, err := os.MkdirTemp("", "testdir.")
+	if err != nil {
+		return "", func() error { return nil }, err
+	}
+
+	err = copyDirFromTo(fromDir, tmpDir)
+	return tmpDir, func() error { return os.RemoveAll(tmpDir) }, err
+}
+
+// copyDirFromTo copies the contents of "from" into "to". It will call itself
+// recursively for subdirectories and return the first error encountered.
+// Both "from" and "to" directories should exist before it is called.
+func copyDirFromTo(from, to string) error {
+	d, err := os.Open(from)
+	if err != nil {
+		return err
+	}
+	defer d.Close()
+
+	toBeCopied, err := d.Readdir(0)
+	if err != nil {
+		return err
+	}
+
+	for _, fi := range toBeCopied {
+		if fi.IsDir() {
+			var (
+				newFromDir = filepath.Join(from, fi.Name())
+				newToDir   = filepath.Join(to, fi.Name())
+			)
+			err = os.Mkdir(newToDir, fi.Mode()&fs.ModePerm)
+			if err != nil {
+				return err
+			}
+			err = copyDirFromTo(newFromDir, newToDir)
+			if err != nil {
+				return err
+			}
+		} else if fi.Mode().IsRegular() {
+			var (
+				fromFile = filepath.Join(from, fi.Name())
+				toFile   = filepath.Join(to, fi.Name())
+			)
+			fromBytes, err := os.ReadFile(fromFile)
+			if err != nil {
+				return err
+			}
+			err = os.WriteFile(toFile, fromBytes, fi.Mode()&fs.ModePerm)
+			if err != nil {
+				return err
+			}
+		} else {
+			return fmt.Errorf(
+				"only dirs & regular files can be copied, %q is neither",
+				fi.Name())
+		}
+	}
+	return nil
 }
